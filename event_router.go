6503bc5969e8b4d43254475b855b6207f3379335
Jason Whitaker <jason@thewhitakers.ca>
2020-02-07 21:20:13 -0500

    Adding some documentation

--- event_router.go
+++ event_router.go
@@ -4,20 +4,24 @@ import "log"
 
 type eventMappings map[string]EventHandler
 
+// EventRouter defines an event router
 type EventRouter struct {
 	mappings eventMappings
 }
 
+// NewEventRouter creates a new event router
 func NewEventRouter() EventRouter {
 	return EventRouter{ eventMappings {} }
 }
 
+// AddHandler adds an event handler for an event
 func (eventRouter EventRouter) AddHandler(event string, eventHandler EventHandler) {
-	log.Printf("Registering handler %s", event, eventHandler)
+	log.Printf("Registering handler for event %s", event)
 
 	eventRouter.mappings[event] = eventHandler
 }
 
+// Events returns a list of handled events
 func (eventRouter EventRouter) Events() []string {
 	eventKeys := make([]string, 0, len(eventRouter.mappings))
 
@@ -28,6 +32,7 @@ func (eventRouter EventRouter) Events() []string {
 	return eventKeys
 }
 
+// GetHandler returns a handler for an event
 func (eventRouter EventRouter) GetHandler(event string) (EventHandler, bool) {
 	handler, ok := eventRouter.mappings[event]
 
